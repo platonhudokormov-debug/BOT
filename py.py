b186c693a601cc5726c604f37b5aa2dda97ac7f3
platonhudokormov <platon.hudokormov@gmail.com>
2026-04-08 15:34:58 +0300

    first commit

--- py.py
+++ py.py
@@ -1,46 +1,36 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 
-# Сюди вставляємо свій токен з BotFather
 API_TOKEN = "8707422337:AAHPEwq-knwmxI_b3vjBAVbmZh461t1L0q0"
 
-# Створюємо бота і диспетчер
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-# Обробник команди /start
+# /start
 @dp.message(CommandStart())
 async def start_handler(message: Message):
-    await message.answer("Привіт! Я твій перший бот!😉 ")
+    await message.answer("Привіт! Я твій перший бот!😉")
 
-# Головна функція для запуску бота
+# /help
+@dp.message(Command("help"))
+async def help_command(message: Message):
+    await message.answer(
+        "Ось що я вмію:\n"
+        "/start - привітання\n"
+        "/help - довідка\n"
+        "/about - про мене"
+    )
+
+# /about
+@dp.message(Command("about"))
+async def about_command(message: Message):
+    await message.answer("Я створений на Python з бібліотекою Aiogram!")
+
+# запуск
 async def main():
     await dp.start_polling(bot)
 
-# Запускаємо
 if __name__ == "__main__":
     asyncio.run(main())
-
-
-     x
-
-    @dp.message(Command("help"))
-    async def help_command(message: Message):
-        await message.answer("Ось що я вмію:\n/start - привітання\n/help - довідка\n/about - про мене")
-
-
-    # Обробник команди /about
-    @dp.message(Command("about"))
-    async def about_command(message: Message):
-        await message.answer("Я створений на Python з бібліотекою Aiogram!")
-
-
-    # Запуск
-    async def main():
-        await dp.start_polling(bot)
-
-
-    if __name__ == "__main__":
-        asyncio.run(main())
