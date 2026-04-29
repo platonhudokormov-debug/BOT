b96c6f7a18332162ce794ee6c8b227c215afc5e7
platonhudokormov <platon.hudokormov@gmail.com>
2026-04-29 15:29:21 +0300

    setings buttons

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.filters import CommandStart, Command
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 API_TOKEN = "8707422337:AAHPEwq-knwmxI_b3vjBAVbmZh461t1L0q0"
 
@@ -20,7 +21,8 @@ async def help_command(message: Message):
         "Ось що я вмію:\n"
         "/start - привітання\n"
         "/help - довідка\n"
-        "/about - про мене"
+        "/about - про мене\n"
+        "/menu - меню"
     )
 
 # /about
@@ -28,6 +30,7 @@ async def help_command(message: Message):
 async def about_command(message: Message):
     await message.answer("Я створений на Python з бібліотекою Aiogram учнем 10-А класу")
 
+# /menu
 @dp.message(Command("menu"))
 async def show_menu(message: Message):
     keyboard = ReplyKeyboardMarkup(
@@ -39,7 +42,7 @@ async def show_menu(message: Message):
     )
     await message.answer("Вибери опцію:", reply_markup=keyboard)
 
-
+# Обробка кнопок
 async def handle_message(message: Message):
     text = message.text
     if text == "Привіт 👋":
@@ -51,17 +54,12 @@ async def handle_message(message: Message):
     else:
         await message.answer("Натисни одну з кнопок 😺")
 
+dp.message.register(handle_message)
+
+# Запуск
 async def main():
     print("Бот запущений...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
-# Налаштування клавіатури
-def main_menu():
-    builder = ReplyKeyboardBuilder()
-    builder.button(text="Як називається кіт-програміст? — JavaMeow!")
-    builder.button(text="Як справи? 😊 ")
-    builder.button(text="Інтернет – така зараза, гірше сємочок в три раза!😏")
-    builder.adjust(2)
-    return builder.as_markup(resize_keyboard=True)
